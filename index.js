45cb27a65278e45e330f1719e63fdc8197fec3ca
Solih Ahmad Sohan <sasohanme@gmail.com>
2022-04-29 01:29:10 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,3 +16,27 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log("connected to", port);
 });
+//mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2nttd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db("book-verse").collection("items");
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+    console.log("connected to mongoDB");
+  } finally {
+  }
+}
+run().catch(console.dir);
